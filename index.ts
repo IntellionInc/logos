d0d60998a15e6805970cd864086b270422da50aa
mmmikolay <tuncer.ilk@gmail.com>
2021-11-28 20:45:51 +0300

    refined index exports

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-export * from "./src";
+export * from "./src/communicator";
+export * from "./src/controller";
+export * from "./src/entities";
+export * from "./src/router";
+export * from "./src/server";
+export * from "./src/types";
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./communicator";
-export * from "./controller";
-export * from "./entities";
-export * from "./router";
-export * from "./server";
-export * from "./types";
