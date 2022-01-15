18afc865a74d96f440050841f5e45e5eb560582a
mmmikolay <tuncer.ilk@gmail.com>
2022-01-15 20:08:07 +0300

    isolated status codes and error messages

--- src/controller/StatusCodes.ts
+++ src/controller/StatusCodes.ts
@@ -1,7 +1,13 @@
 export class STATUS {
 	static SUCCESS = 200;
 	static BAD_REQUEST = 400;
+	static UNAUTHORIZED = 401;
 	static NOT_FOUND = 404;
 	static UNPROCESSABLE_ENTITY = 422;
 	static INTERNAL_SERVER_ERROR = 500;
+}
+
+export class ERROR_MESSAGES {
+	static UNAUTHORIZED = "Unauthorized";
+	static BAD_REQUEST = "Invalid arguments";
 }
--- test/controller/interceptors/DerivedInterceptors.spec.ts
+++ test/controller/interceptors/DerivedInterceptors.spec.ts
@@ -5,21 +5,34 @@ import {
 	ValidationInterceptor
 } from "src/controller";
 
+import { STATUS, ERROR_MESSAGES } from "src/controller/StatusCodes";
+
+jest.mock("src/controller/StatusCodes", () => ({
+	STATUS: class MockSTATUS {
+		static BAD_REQUEST = 400;
+		static UNAUTHORIZED = 401;
+	},
+	ERROR_MESSAGES: class ERROR_MESSAGES {
+		static BAD_REQUEST = "Invalid arguments";
+		static UNAUTHORIZED = "Unauthorized";
+	}
+}));
+
 describe("Derived Interceptors: ", () => {
 	[
 		{
 			ctx: "AuthInterceptor: ",
 			SubClass: AuthInterceptor,
 			controllerProtocol: "authProtocol",
-			failureStatus: 401,
-			failureMessage: "Unauthorized"
+			failureStatus: STATUS.UNAUTHORIZED,
+			failureMessage: ERROR_MESSAGES.UNAUTHORIZED
 		},
 		{
 			ctx: "ValidationInterceptor: ",
 			SubClass: ValidationInterceptor,
 			controllerProtocol: "validationProtocol",
-			failureStatus: 400,
-			failureMessage: "Invalid arguments"
+			failureStatus: STATUS.BAD_REQUEST,
+			failureMessage: ERROR_MESSAGES.BAD_REQUEST
 		}
 	].forEach(({ ctx, SubClass, controllerProtocol, failureStatus, failureMessage }) => {
 		describe(ctx, () => {
@@ -36,7 +49,7 @@ describe("Derived Interceptors: ", () => {
 				expect(uut).toBeDefined();
 				expect(uut).toBeInstanceOf(SubClass);
 			});
-			it("should extend 'Interceptor'", () => {
+			it("should extend 'BaseInterceptor'", () => {
 				expect(uut).toBeInstanceOf(BaseInterceptor);
 			});
 
