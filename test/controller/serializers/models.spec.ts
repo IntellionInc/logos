f0ae4fb11bd78550f9e3697c840d3fe27944073f
mmmikolay <tuncer.ilk@gmail.com>
2022-02-15 17:55:43 +0300

    refactored test to use 'describe.each'

--- test/controller/serializers/models.spec.ts
+++ test/controller/serializers/models.spec.ts
@@ -1,8 +1,8 @@
 import * as Models from "src/controller/models";
 
-type ModelName = "String" | "Email" | "Number" | "Boolean";
+type TypeName = "String" | "Email" | "Number" | "Boolean";
 
-describe("IntellionType", () => {
+describe("IntellionType: ", () => {
 	beforeAll(() => {
 		jest.useFakeTimers();
 	});
@@ -30,64 +30,64 @@ describe("IntellionType", () => {
 	});
 });
 
-[
+const testedTypes = [
 	{
-		name: "String",
+		typeName: "String",
 		definition: "a string",
 		match: "some-string",
 		mismatch: 12345
 	},
 	{
-		name: "Email",
+		typeName: "Email",
 		definition: "a string that contains an @ sign",
 		match: "some-mail@",
 		mismatch: "some-string"
 	},
 	{
-		name: "Number",
+		typeName: "Number",
 		definition: "a number",
 		match: 12345,
 		mismatch: "some-string"
 	},
 	{
-		name: "Boolean",
+		typeName: "Boolean",
 		definition: "a boolean",
 		match: false,
 		mismatch: "some-string"
 	},
 	{
-		name: "Date",
-		definition: "a boolean",
+		typeName: "Date",
+		definition: "a date instance",
 		match: new Date(1609459200000), // 2021-01-01
 		mismatch: "Thu Jan 01 1970 02:00:00 GMT+0200 (GMT+03:00)"
 	}
-].forEach(({ name, definition, match, mismatch }) => {
-	describe(name, () => {
-		const uut = Models[name as ModelName];
-		const instance = new uut();
+];
 
-		it("should extend 'IntellionType", () => {
-			expect(instance).toBeInstanceOf(Models.IntellionType);
+describe.each(testedTypes)("$typeName: ", ({ typeName, definition, match, mismatch }) => {
+	const uut = Models[typeName as TypeName];
+	const instance = new uut();
+
+	it("should extend 'IntellionType", () => {
+		expect(instance).toBeInstanceOf(Models.IntellionType);
+	});
+
+	describe("class properties", () => {
+		describe("definition", () => {
+			it("should have the correct description", () => {
+				expect(uut.definition).toBe(definition);
+			});
 		});
 
-		describe("class properties", () => {
-			describe("definition", () => {
-				it("should have the correct description", () => {
-					expect(uut.definition).toBe(definition);
+		describe("hasSameTypeAs", () => {
+			describe("when there is no type mismatch", () => {
+				it("should return true", () => {
+					expect(uut.hasSameTypeAs(match)).toBe(true);
 				});
 			});
 
-			describe("hasSameTypeAs", () => {
-				describe("when there is no type mismatch", () => {
-					it("should return true", () => {
-						expect(uut.hasSameTypeAs(match)).toBe(true);
-					});
-				});
-
-				describe("when there is a type mismatch", () => {
-					it("should return false", () => {
-						expect(uut.hasSameTypeAs(mismatch)).toBe(false);
-					});
+			describe("when there is a type mismatch", () => {
+				it("should return false", () => {
+					expect(uut.hasSameTypeAs(mismatch)).toBe(false);
 				});
 			});
 		});
