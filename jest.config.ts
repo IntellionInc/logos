dfa1a9d340c029fc9bf8108b40e486b3c4e94e69
smuuti <umutbekci94@gmail.com>
2022-10-05 15:08:23 +0300

    testEnvironment included in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -6,5 +6,6 @@ export default {
 	testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
 	transform: {
 		"^.+\\.(ts|tsx)$": "ts-jest"
-	}
+	},
+	testEnvironment: "jsdom"
 };
